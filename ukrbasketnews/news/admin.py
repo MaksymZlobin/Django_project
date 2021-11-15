227eb8495cfb8bbb9e5d2d68a015b467dbdd8536
Maksym Zlobin <maksym.zlobin.python@gmail.com>
2021-11-15 21:19:31 +0200

    User model added. Custom user manager added. Migration needed. Settings updated with AUTH_USER_MODEL declaration.

--- ukrbasketnews/news/admin.py
+++ ukrbasketnews/news/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from news.models import Article, Comment
+from news.models import Article, Comment, User
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
 
 
 @admin.register(Article)
@@ -10,3 +12,24 @@ class ArticleAdmin(admin.ModelAdmin):
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('id', 'article', 'author_name', 'comment_text')
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('id', 'email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ('email', 'id',)
