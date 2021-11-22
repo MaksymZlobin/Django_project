97aa29d0350a8372678b756568829f59289bddc0
Maksym Zlobin <maksym.zlobin.python@gmail.com>
2021-11-22 22:51:19 +0200

    Url for articles-list view added. Html templates modified. Added new parameters in main.css file.

--- ukrbasketnews/main/urls.py
+++ ukrbasketnews/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from main import views
+from news.views import articles_list
 
 urlpatterns = [
     path('', views.main_page, name='main'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('articles_list/', articles_list, name='articles'),
 ]
