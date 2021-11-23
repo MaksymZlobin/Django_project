f1f70d307df040b32bb043487241ea82eeb94af8
Maksym Zlobin <maksym.zlobin.python@gmail.com>
2021-11-23 19:55:20 +0200

    Changed url path for articles list in navigation menu. Layout and list htmls updated.

--- ukrbasketnews/main/urls.py
+++ ukrbasketnews/main/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from main import views
-from news.views import articles_list
 
 urlpatterns = [
     path('', views.main_page, name='main'),
     path('about/', views.about, name='about'),
-    path('articles_list/', articles_list, name='articles'),
 ]
